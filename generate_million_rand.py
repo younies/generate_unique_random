828b851de3e33cd91a76d2fd8ac9326de8533959
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-07-22 07:11:44 -0500

    push

---
+++ generate_million_rand.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import random
+
+
+randList = []
+
+maxnumber = 8616864326 - 10
+
+whatweneed = 10000000
+
+randList = [0] * (whatweneed * 4)
+for i in range(whatweneed * 4):
+	randList[i] = random.randint(0,maxnumber)
+
+
+randList = sorted(randList)
+
+uniqueRandList = [randList[0]]
+
+for i in range(1 , len(randList)):
+	if(randList[i] != randList[i-1]):
+		uniqueRandList.append(randList[i])
+
+random.shuffle(uniqueRandList)
+
+uniqueRandList = uniqueRandList[:whatweneed]
+
+for num in uniqueRandList:
+	print num 
